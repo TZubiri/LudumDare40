e917c34546f1a8ccd1c7ceefd61e95f347c4b06a
TZubiri <tomasyorke@hotmail.com>
2017-12-02 01:56:17 -0300

    init

---
+++ Santa Crawl/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position  = new Vector3(transform.position.x, transform.position.y + 1 * Time.deltaTime);
+        }
+        else if (!Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 1 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x + 1 * Time.deltaTime,transform.position.y);
+        }
+        else if (!Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - 1 * Time.deltaTime,transform.position.y);
+        }
+
+    }
+}
